1689e7401030fae340e2c89412e1c00b77ba1c93
yudhapratamadev <yudhaap700@gmail.com>
2024-12-22 02:00:46 +0700

    mengganti marker menjadi lottieanimation

--- app/src/main/java/com/pratama/myservice/MainActivity.java
+++ app/src/main/java/com/pratama/myservice/MainActivity.java
@@ -2,21 +2,13 @@ package com.pratama.myservice;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.Build;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -24,70 +16,31 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-
     private MapView mapView;
     private GoogleMap mMap;
     private TextView tanggalgempa,waktugempa,srgempa,kedalamangempa,lokasigempa,statusgempa;
     private String koordinat,tanggal,jam,wilayah,sr,kedalaman,potensi;
     private LatLng latLng;
     private SwipeRefreshLayout swipeRefreshLayout;
-    private Handler mHandler = new Handler();
-    private int mInterval = 100; // Interval waktu dalam milidetik antara perubahan gambar marker
-    private int[] mFrames = { R.drawable.quake1, R.drawable.quake2, R.drawable.quake3, R.drawable.quake4, R.drawable.quake5, R.drawable.quake6, R.drawable.quake7 }; // Daftar frame GIF
     private ImageView mn1,mn2,mn3,mn4,mn5,mn6,mn7,mn8,mn9;
-    private Marker mMarker;
-    private int targetWidth = 150; // Lebar target gambar marker (sesuaikan dengan kebutuhan Anda)
-    private int targetHeight = 150; // Tinggi target gambar marker (sesuaikan dengan kebutuhan Anda)
-
-
+    private LottieAnimationView lottieMarker;
     private RequestQueue queue;
-//    private NotificationManager notificationManager;
-//    private static final int NOTIFICATION_ID = 123;
-//    private static final String CHANNEL_ID = "Channel_Id";
-
-    // Metode untuk mengganti gambar marker
-    // Metode untuk mengganti gambar marker
-    private Runnable mRunnable = new Runnable() {
-        int frameIndex = 0;
-
-        @Override
-        public void run() {
-            if (mMarker != null) {
-                int drawableId = mFrames[frameIndex];
-                Bitmap resizedBitmap = resizeBitmap(drawableId, targetWidth, targetHeight);
-                mMarker.setIcon(BitmapDescriptorFactory.fromBitmap(resizedBitmap));
-                frameIndex = (frameIndex + 1) % mFrames.length;
-                mHandler.postDelayed(this, mInterval);
-            }
-        }
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,117 +66,50 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         queue = Volley.newRequestQueue(this);
         loadDataFromApi();
         refresh();
-
-        //notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Memulai polling untuk memeriksa pembaruan setiap 10 detik
-        //startPolling();
-
     }
 
-
-
     private void goToTsunami() {
-        mn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Edu_tsunami.class));
-            }
-        });
+        mn1.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Edu_tsunami.class)));
     }
 
     private void goToGunungMeletus() {
-        mn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Edu_gunungmeletus.class));
-            }
-        });
+        mn2.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Edu_gunungmeletus.class)));
     }
 
     private void goToLongsor() {
-        mn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Edu_longsor.class));
-            }
-        });
+        mn3.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Edu_longsor.class)));
     }
 
     private void goToBanjir() {
-        mn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Edu_banjir.class));
-            }
-        });
+        mn4.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Edu_banjir.class)));
     }
 
     private void goToAnginTopan() {
-        mn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Edu_angintopan.class));
-            }
-        });
+        mn5.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Edu_angintopan.class)));
     }
 
     private void goToKebakaran() {
-        mn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Edu_kebakaran.class));
-            }
-        });
+        mn6.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Edu_kebakaran.class)));
     }
 
     private void goToKekeringan() {
-        mn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Edu_kekeringan.class));
-            }
-        });
+        mn7.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Edu_kekeringan.class)));
     }
 
     private void goToGempa() {
-        mn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Edu_gempabumi.class));
-            }
-        });
+        mn8.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Edu_gempabumi.class)));
     }
 
     private void goToWabah() {
-        mn9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Edu_wabah.class));
-            }
-        });
+        mn9.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Edu_wabah.class)));
     }
 
     private void refresh() {
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                swipeRefreshLayout.setRefreshing(false);
-                loadDataFromApi();
-            }
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            swipeRefreshLayout.setRefreshing(false);
+            loadDataFromApi();
         });
     }
-
-//    private void startPolling() {
-//        Timer timer = new Timer();
-//        timer.scheduleAtFixedRate(new TimerTask() {
-//            @Override
-//            public void run() {
-//                // Memuat data JSON dari URL API menggunakan Volley
-//                loadDataFromApi();
-//            }
-//        }, 0, 10000); // Memeriksa setiap 10 detik
-//    }
 
     private void initV() {
         tanggalgempa = findViewById(R.id.tanggal);
@@ -233,6 +119,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         lokasigempa = findViewById(R.id.lokasi);
         statusgempa = findViewById(R.id.status);
         swipeRefreshLayout = findViewById(R.id.refreshmain);
+        lottieMarker = findViewById(R.id.lottieMarker);
 
         mn1 = findViewById(R.id.mn_tsunami);
         mn2 = findViewById(R.id.mn_gunungmeletus);
@@ -254,116 +141,82 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         progressDialog.show();
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // Menangani respon JSON dari server
-                        try {
-                            JSONObject infogempa = response.getJSONObject("Infogempa").getJSONObject("gempa");
-                            tanggal = infogempa.getString("Tanggal");
-                            jam = infogempa.getString("Jam");
-                            koordinat = infogempa.getString("Coordinates");
-                            sr = infogempa.getString("Magnitude");
-                            kedalaman = infogempa.getString("Kedalaman");
-                            wilayah = infogempa.getString("Wilayah");
-                            potensi = infogempa.getString("Potensi");
+                response -> {
+                    // respon JSON dari server
+                    try {
+                        JSONObject infogempa = response.getJSONObject("Infogempa").getJSONObject("gempa");
+                        tanggal = infogempa.getString("Tanggal");
+                        jam = infogempa.getString("Jam");
+                        koordinat = infogempa.getString("Coordinates");
+                        sr = infogempa.getString("Magnitude");
+                        kedalaman = infogempa.getString("Kedalaman");
+                        wilayah = infogempa.getString("Wilayah");
+                        potensi = infogempa.getString("Potensi");
 
+                        progressDialog.dismiss();
+                            String[] parts = koordinat.split(",");
+                            double latitude = Double.parseDouble(parts[0]);
+                            double longitude = Double.parseDouble(parts[1]);
+                            latLng = new LatLng(latitude, longitude);
+                            tanggalgempa.setText(tanggal);
+                            waktugempa.setText(jam);
+                            srgempa.setText("Kekuatan " + sr + " SR");
+                            kedalamangempa.setText("Kedalaman " + kedalaman);
+                            lokasigempa.setText(wilayah);
+                            statusgempa.setText(potensi);
 
-
-                            // Memeriksa pembaruan data
-                            // Untuk sederhana, kita memeriksa apakah tanggal gempa telah berubah
-                            // Anda dapat memeriksa lebih banyak detail sesuai kebutuhan Anda
-
-                           // if (!Objects.equals(jam, waktugempa.getText().toString())) {
-                                // Mendapatkan nilai lintang dan bujur dari string koordinat
-                            progressDialog.dismiss();
-                                String[] parts = koordinat.split(",");
-                                double latitude = Double.parseDouble(parts[0]);
-                                double longitude = Double.parseDouble(parts[1]);
-                                latLng = new LatLng(latitude, longitude);
-                                tanggalgempa.setText(tanggal);
-                                waktugempa.setText(jam);
-                                srgempa.setText("Kekuatan " + sr + " SR");
-                                kedalamangempa.setText("Kedalaman " + kedalaman);
-                                lokasigempa.setText(wilayah);
-                                statusgempa.setText(potensi);
-                                // Menampilkan notifikasi
-                                //showNotification("Info Gempa", "Ada pembaruan data gempa");
-                            //}
-
-                            // Menampilkan lokasi gempa di peta setelah mendapatkan koordinat
-                            if (mMap != null && latLng != null) {
-                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7),6000,null);
-                                addMarkerToLocation(latLng);
-                            }
-
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Toast.makeText(MainActivity.this, "error "+e, Toast.LENGTH_SHORT).show();
+                        // Menampilkan lokasi gempa di peta setelah mendapatkan koordinat
+                        if (mMap != null && latLng != null) {
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5),7000,null);
+                            addMarkerToLocation(latLng);
+                            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
                         }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "error "+e, Toast.LENGTH_SHORT).show();
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // Menangani kesalahan saat permintaan
-                progressDialog.dismiss();
-                Toast.makeText(MainActivity.this, "Gagal Menampilkan Data, Periksa Jaringan", Toast.LENGTH_SHORT).show();
-            }
-        });
+                }, error -> {
+                    // Menangani kesalahan saat permintaan
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Gagal Menampilkan Data, Periksa Jaringan", Toast.LENGTH_SHORT).show();
+                });
 
         // Menambahkan permintaan ke antrian
         queue.add(jsonObjectRequest);
     }
 
-//    private void showNotification(String title, String message) {
-//        // Membuat Intent untuk menavigasi ke aplikasi Anda saat notifikasi diklik
-//        Intent intent = new Intent(this, MainActivity.class);
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
-//
-//        // Membuat kanal notifikasi untuk Android Oreo (API level 26) ke atas
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Channel Name", NotificationManager.IMPORTANCE_DEFAULT);
-//            channel.setDescription("Channel Description");
-//            channel.enableLights(true);
-//            channel.setLightColor(Color.RED);
-//            notificationManager.createNotificationChannel(channel);
-//        }
-
-        // Membuat dan menampilkan notifikasi
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-//                .setSmallIcon(R.drawable.baseline_notifications_24)
-//                .setContentTitle(title)
-//                .setContentText(message)
-//                .setContentIntent(pendingIntent)
-//                .setAutoCancel(true);
-//
-//        // Membatalkan notifikasi sebelum menampilkan yang baru
-//        notificationManager.cancel(NOTIFICATION_ID);
-//        notificationManager.notify(NOTIFICATION_ID, builder.build());
-//    }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         // Inisialisasi GoogleMap
         mMap = googleMap;
+        lottieMarker.setVisibility(View.VISIBLE);
+        mMap.setOnCameraMoveListener(() -> {
+            if (latLng != null) {
+                updateLottiePosition(latLng);
+            }
+        });
 
     }
 
-    private Marker addMarkerToLocation(LatLng latLng) {
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        // Resize gambar sebelum digunakan sebagai ikon marker
-        //markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+    private void updateLottiePosition(LatLng latLng) {
+        if (mMap != null) {
+            //mMap.addMarker(new MarkerOptions().position(latLng).title("Lokasi Gempa"));
 
-//        Bitmap resizedBitmap = resizeBitmap(mFrames, targetWidth, targetHeight); // Anda dapat memilih frame awal atau yang sesuai dengan kebutuhan Anda
-//        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(resizedBitmap));
+            Point screenPosition = mMap.getProjection().toScreenLocation(latLng);
+            lottieMarker.setX(screenPosition.x - (lottieMarker.getWidth() / 2f));
+            lottieMarker.setY(screenPosition.y - (lottieMarker.getHeight() / 2f));
+        }
+    }
 
-        mMarker = mMap.addMarker(markerOptions);
-        // Mulai animasi
-        mHandler.postDelayed(mRunnable, mInterval);
-        return mMap.addMarker(markerOptions);
+    private void addMarkerToLocation(LatLng latLng) {
+        if (mMap != null) {
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Lokasi Gempa"));
+
+            Point screenPosition = mMap.getProjection().toScreenLocation(latLng);
+            lottieMarker.setX(screenPosition.x - (lottieMarker.getWidth() / 2f));
+            lottieMarker.setY(screenPosition.y - (lottieMarker.getHeight() / 2f));
+        }
     }
 
     private Bitmap resizeBitmap(int resId, int width, int height) {
